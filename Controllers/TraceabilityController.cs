28ab0e8e9348af8c78e8232404c04bef6cddc41b
RedefinitionLQ <19921305202@163.com>
2022-06-24 00:16:40 +0800

    修复密码泄露的bug

---
+++ Controllers/TraceabilityController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataWarehouse.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TraceabilityController : ControllerBase
+    {
+    }
+}
